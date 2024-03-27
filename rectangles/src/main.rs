a4550213450ecb1a54ba8dab67c8ac536d74ea19
computer-mann <hpsn16@gmail.com>
2024-03-27 02:22:21 +0000

    leave my mind2

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,18 @@
+struct Rectanle{
+    width:u32,
+    height:u32
+}
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    let rec=Rectanle{
+        width: 30,
+        height: 40
+    };
+    println!("{}",area(&rec))
+
+}
+
+fn area(rect:&Rectanle)->u32{
+    rect.width * rect.height
 }
